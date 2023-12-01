4d45fa80e901bacc7b5ae06ef1c1fc536ec1028a
MayOwall <pixartive@gmail.com>
2023-12-01 21:51:14 +0900

    3. 컴포넌트

--- basic/src/components/MyComponent.jsx
+++ basic/src/components/MyComponent.jsx
@@ -1,20 +1,26 @@
+import { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name }) => {
-  return (
-    <>
-      <div>The limits of my language means the limits of my world.</div>
-      <div>- {name}</div>
-    </>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "아무개",
+  };
 
-MyComponent.defaultProps = {
-  name: "아무개",
-};
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
 
-MyComponent.propTypes = {
-  name: PropTypes.string.isRequired,
-};
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <>
+        <div>안녕하세요 제 이름은 {name}입니다</div>
+        <div>제가 좋아하는 숫자는 {favoriteNumber}입니다</div>
+        <div>{children}</div>
+      </>
+    );
+  }
+}
 
 export default MyComponent;
