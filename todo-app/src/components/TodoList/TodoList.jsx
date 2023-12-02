6337444da84602170354abb34064b7cb08d718ed
MayOwall <pixartive@gmail.com>
2023-12-03 08:50:55 +0900

    todo-app: TodoList 컴포넌트 구현

---
+++ todo-app/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,14 @@
+import { TodoListItem } from '../index';
+import './TodoList.scss';
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+
+export default TodoList;
